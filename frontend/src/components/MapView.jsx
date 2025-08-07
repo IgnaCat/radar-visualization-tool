ffd95f976d59975f19ec23a7c20f1da7ac15b235
Igna Martinez <itsigna02@gmail.com>
2025-08-07 16:03:24 -0300

    add animation and multiple radar files

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -6,27 +6,15 @@ export default function MapView({ overlayData }) {
     <MapContainer
       center={[-34.6, -58.4]}
       zoom={5}
-      style={{ height: "100vh", width: "100%" }}
+      style={{ height: "100vh", width: "105%" }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
-      {overlayData?.animation ? (
-        // Si hay animación, mostrarla ocupando toda la región de Argentina
+      {overlayData != null && (
         <ImageOverlay
-          url={`http://localhost:5000${overlayData.overlays[0].image_url}`}
-          bounds={overlayData.overlays[0].bounds}
-          opacity={0.8}
+          url={"http://localhost:5000" + overlayData.image_url}
+          bounds={overlayData.bounds}
+          opacity={0.9}
         />
-      ) : (
-        // Si no hay animación, mostrar cada overlay individual
-        overlayData?.overlays?.map((overlay, idx) => (
-          <ImageOverlay
-            key={idx}
-            url={`http://localhost:5000${overlay.image_url}`}
-            bounds={overlay.bounds}
-            opacity={0.8}
-          />
-        ))
       )}
     </MapContainer>
   );
