67c9561e7d7bf5b870c673092dc407e575bcc8bf
Igna Martinez <itsigna02@gmail.com>
2025-09-06 12:59:36 -0300

    fix(frontend): change color legend

--- frontend/src/components/ColorLegend.jsx
+++ frontend/src/components/ColorLegend.jsx
@@ -1,31 +1,88 @@
 export default function ColorLegend() {
   const legend = [
-    { value: 70, color: "purple", label: "Lluvia muy intensa y granizo" },
-    { value: 60, color: "red", label: "Lluvia muy intensa y granizo" },
-    { value: 50, color: "orange", label: "Lluvia intensa" },
-    { value: 40, color: "yellow", label: "Lluvia intensa" },
-    { value: 30, color: "green", label: "Lluvia moderada" },
-    { value: 20, color: "blue", label: "Lluvia leve" },
-    { value: 10, color: "lightblue", label: "Llovisna" },
-    { value: 0, color: "gray", label: "Neblina" },
-    { value: -10, color: "white", label: "Nubes no precipitantes" },
+    {
+      color: "grey",
+      label: "COLMAX(Z)",
+      letter_color: "black",
+    },
+    {
+      value: 70,
+      color: "#FF29E3",
+      label: "Lluvia muy intensa y granizo",
+      letter_color: "white",
+    },
+    {
+      value: 60,
+      color: "#FF2A98",
+      label: "Lluvia muy intensa y granizo",
+      letter_color: "white",
+    },
+    {
+      value: 50,
+      color: "#FF2A0C",
+      label: "Lluvia intensa",
+      letter_color: "white",
+    },
+    {
+      value: 40,
+      color: "#f7a600",
+      label: "Lluvia intensa",
+      letter_color: "black",
+    },
+    {
+      value: 30,
+      color: "#EAF328",
+      label: "Lluvia moderada",
+      letter_color: "black",
+    },
+    {
+      value: 20,
+      color: "#00AD5A",
+      label: "Lluvia leve",
+      letter_color: "black",
+    },
+    { value: 10, color: "#00E68A", label: "Llovizna", letter_color: "black" },
+    { value: 0, color: "#95b4dc", label: "Neblina", letter_color: "black" },
+    {
+      value: -10,
+      color: "#f0f6f2",
+      label: "Nubes no precipitantes",
+      letter_color: "black",
+    },
   ];
 
   return (
     <div
       style={{
         position: "absolute",
-        right: 10,
-        top: 100,
+        left: 40,
+        bottom: 10,
         zIndex: 1000,
-        background: "rgba(255,255,255,0.8)",
-        padding: "10px",
-        borderRadius: "8px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "5px",
       }}
     >
       {legend.map((item) => (
-        <div key={item.value} style={{ color: item.color }}>
-          {item.value}: {item.label || ""}
+        <div
+          key={item.value}
+          title={`${item.label}`}
+          style={{
+            width: "26px",
+            height: "26px",
+            borderRadius: "50%",
+            backgroundColor: item.color,
+            cursor: "pointer",
+
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            fontWeight: "bold",
+            color: item.letter_color,
+            fontSize: "16px",
+          }}
+        >
+          {item.value || item.value === 0 ? item.value : "?"}
         </div>
       ))}
     </div>
