34864912882d503499d655b109ac91a086e141eb
Igna Martinez <itsigna02@gmail.com>
2025-09-17 17:44:11 -0300

    feat(backend): add util to display radar on pngs

---
+++ backend/app/utils/png.py
@@ -0,0 +1,78 @@
+
+import os
+import pyart
+import matplotlib
+matplotlib.use("Agg") # Use non-interactive backend for matplotlib
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import uuid
+from .colores import get_cmap_grc_th
+
+
+def create_png(radar, product, output_dir, field_used, filters=[], elevation=0):
+    """
+    Genera una imagen PNG a partir de un objeto Py-ART Radar.
+    Retorna el path del archivo generado.
+    """
+
+    # Crear path único para la imagen
+    os.makedirs(output_dir, exist_ok=True)
+    aux = "_".join([f"{a[0]}{a[1]}" for a in filters]) if filters else "nofilter"
+    unique_name = f"png_{product}_{elevation}_{aux}_{uuid.uuid4().hex}.png"
+    output_path = os.path.join(output_dir, unique_name)
+
+    fig = plt.figure(figsize=[15, 10])
+    ax = plt.axes(projection=ccrs.Mercator()) #PlateCarree
+    display = pyart.graph.RadarMapDisplay(radar)
+
+    # Aplicar filtros si se proporcionan
+    gf = pyart.filters.GateFilter(radar)
+    gf.exclude_transition()
+    for filter in filters:
+        if filter[0] in radar.fields:
+            gf.exclude_below(filter[0], filter[1])
+
+    if field_used == 'ppi':
+        display.plot_ppi_map(field_used,
+                        sweep=elevation,
+                        vmin=-30,
+                        vmax=70,
+                        projection=ccrs.Mercator(),
+                        ax=ax,
+                        colorbar_flag=False,
+                        cmap=get_cmap_grc_th(),
+                        gatefilter=gf)
+    else:
+        display.plot_ppi_map(field_used,
+                        vmin=-30,
+                        vmax=70,
+                        projection=ccrs.Mercator(),
+                        ax=ax,
+                        colorbar_flag=False,
+                        cmap=get_cmap_grc_th(),
+                        gatefilter=gf)
+    
+
+    # Calcular bounds
+    lat = radar.gate_latitude['data']
+    lon = radar.gate_longitude['data']
+    lat_min, lat_max = float(lat.min()), float(lat.max())
+    lon_min, lon_max = float(lon.min()), float(lon.max())
+    bounds = [[lat_min, lon_min], [lat_max, lon_max]]
+
+    # Eliminar límites del mapa
+    ax.set_title("")
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+
+
+    plt.savefig(output_path, dpi=150, bbox_inches='tight', pad_inches=0, transparent=True )
+    plt.close()
+
+    return output_path
