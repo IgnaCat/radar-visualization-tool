9494602ac3b80827fcbf6e3fb1e818e950805885
Igna Martinez <itsigna02@gmail.com>
2025-12-02 09:51:20 -0300

    feat(frontend): add map images

--- frontend/src/components/BaseMapSelector.jsx
+++ frontend/src/components/BaseMapSelector.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import {
   Box,
   List,
@@ -13,6 +12,15 @@ import {
 import CloseIcon from "@mui/icons-material/Close";
 import CheckIcon from "@mui/icons-material/Check";
 
+// Import images
+import argenmapImg from "../assets/argenmap.webp";
+import argenmapGrisImg from "../assets/argenmap-gris.webp";
+import argenmapOscuroImg from "../assets/argenmap-oscuro.webp";
+import argenmapTopoImg from "../assets/argenmap-topo.webp";
+import esriImg from "../assets/esri.webp";
+import esritImg from "../assets/esrit.webp";
+import bingImg from "../assets/bing.webp";
+
 const baseMaps = [
   {
     id: "osm",
@@ -20,6 +28,7 @@ const baseMaps = [
     url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    image: argenmapImg,
   },
   {
     id: "argenmap",
@@ -27,6 +36,7 @@ const baseMaps = [
     url: "https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png",
     attribution:
       '<a href="https://www.ign.gob.ar/AreaServicios/Argenmap/IntroduccionV2" target="_blank">Instituto Geográfico Nacional</a> + <a href="https://www.osm.org/copyright" target="_blank">OpenStreetMap</a>',
+    image: argenmapImg,
   },
   {
     id: "argenmap-gris",
@@ -34,6 +44,7 @@ const baseMaps = [
     url: "https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/mapabase_gris@EPSG%3A3857@png/{z}/{x}/{-y}.png",
     attribution:
       "<a href='https://www.ign.gob.ar/AreaServicios/Argenmap/IntroduccionV2' target='_blank'>Instituto Geográfico Nacional</a> + <a href='https://www.osm.org/copyright' target='_blank'>OpenStreetMap</a>",
+    image: argenmapGrisImg,
   },
   {
     id: "argenmap-oscuro",
@@ -41,6 +52,7 @@ const baseMaps = [
     url: "https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/argenmap_oscuro@EPSG%3A3857@png/{z}/{x}/{-y}.png", // Cambiar por URL real
     attribution:
       "<a href='https://www.ign.gob.ar/AreaServicios/Argenmap/IntroduccionV2' target='_blank'>Instituto Geográfico Nacional</a> + <a href='https://www.osm.org/copyright' target='_blank'>OpenStreetMap</a>",
+    image: argenmapOscuroImg,
   },
   {
     id: "topografico",
@@ -48,24 +60,28 @@ const baseMaps = [
     url: "https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/mapabase_topo@EPSG%3A3857@png/{z}/{x}/{-y}.png",
     attribution:
       "<a href='https://www.ign.gob.ar/AreaServicios/Argenmap/IntroduccionV2' target='_blank'>Instituto Geográfico Nacional</a> + <a href='https://www.osm.org/copyright' target='_blank'>OpenStreetMap</a>",
+    image: argenmapTopoImg,
   },
   {
     id: "satellite",
     name: "Imágenes satelitales Esri",
     url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
     attribution: "Esri",
+    image: esriImg,
   },
   {
     id: "topo-esri",
     name: "Mapa topográfico Esri",
     url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Physical_Map/MapServer/tile/{z}/{y}/{x}",
     attribution: "Esri",
+    image: esritImg,
   },
   {
     id: "ocean",
     name: "Mapa Esri Fondo Oceánico",
     url: "https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}",
     attribution: "Esri",
+    image: esritImg,
   },
 ];
 
@@ -162,6 +178,21 @@ export default function BaseMapSelector({
                     },
                   }}
                 >
+                  {map.image && (
+                    <Box
+                      component="img"
+                      src={map.image}
+                      alt={map.name}
+                      sx={{
+                        width: 50,
+                        height: 40,
+                        borderRadius: "4px",
+                        marginRight: 2,
+                        objectFit: "cover",
+                        border: "1px solid rgba(0, 0, 0, 0.1)",
+                      }}
+                    />
+                  )}
                   <ListItemText
                     primary={map.name}
                     primaryTypographyProps={{
